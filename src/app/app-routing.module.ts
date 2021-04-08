087dba918f94b1a8ccdf3be630c30595f4967e63
Alan <alanyd.ph@gmail.com>
2021-04-08 02:40:22 -0300

    linkeado poke details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { PokeListComponent } from './components/poke-list/poke-list.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import {PokemonDetail} from "./models/pokemon-detail.model"
+import { PokeDetailComponent } from './components/poke-detail/poke-detail.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: 'home',component:PokeListComponent},
   { path: 'login', component: LoginComponent },
-  { path: 'poke-detail', component: LoginComponent },
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'poke-detail/:id', component: PokeDetailComponent },
   { path: 'login', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home'}
--- src/app/models/pokemon-detail.model.ts
+++ src/app/models/pokemon-detail.model.ts
@@ -1,5 +1,5 @@
 import { PokemonResume } from "./pokemon-resume.model";
 
-export class PokemonDetal extends PokemonResume{
+export class PokemonDetail extends PokemonResume{
 
 }
